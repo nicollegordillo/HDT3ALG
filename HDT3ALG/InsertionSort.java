9625a0ec9a4025a8b5b040391027cd79290a0a61
anapaunavas <106564616+anapaunavas@users.noreply.github.com>
2023-02-06 20:30:34 -0600

    InsertionSort

---
+++ HDT3ALG/InsertionSort.java
@@ -0,0 +1,16 @@
+import java.util.Comparator;
+
+public class InsertionSort<T> {
+    public void quickSort(int[] miarray, Comparator<T> myCompare) {
+        int auxiliar;
+        int counter1;
+        int counter2;
+        for(counter1=1; counter1<miarray.length; counter1++){
+            auxiliar=miarray[counter1];
+            for(counter2=counter1-1; counter2 >=0 && miarray[counter2]>auxiliar; counter2++){
+                miarray[counter2+1]=miarray[counter2];
+                miarray[counter2]=auxiliar;
+            }
+        }
+}
+}
